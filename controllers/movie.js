2c27e7305e3fec4f7d9fda7386f194c410e442a0
Selkyn Kimery <sylfra09@gmail.com>
2024-10-02 12:22:00 +0200

    new repository

--- controllers/movie.js
+++ controllers/movie.js
@@ -43,32 +43,5 @@ exports.searchMovie = async (req, res, next) => {
 
 exports.getMovieDetails
 
-// exports.getRandomMovies = async (req, res, next) => {
-//     try {
-//         const randomMovies = [];
-
-//         for (let i = 0; i < 5; i++) {
-//             // Generate a random IMDb ID (you can customize this as needed)
-//             const randomImdbId = Math.random().toString(36).substring(7);
-
-//             // Make a request to the OMDB API with the random IMDb ID
-//             const response = await axios.get(`https://www.omdbapi.com/?i=${randomImdbId}&apikey=563e52c9`);
-
-//             // Check if the response is successful and contains movie details
-//             if (response.data && response.data.Title) {
-//                 // Push the movie details into the array
-//                 randomMovies.push(response.data);
-//             } else {
-//                 console.error(`No movie details found for the random IMDb ID ${randomImdbId}.`);
-//             }
-//         }
-
-//         // Send the array of movies as the response
-//         res.render('/');
-//     } catch (error) {
-//         console.error('Error fetching random movies:', error.message);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// };
         
 
